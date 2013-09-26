df45a34b61d64eb5a1ddadd776e39bcdf7b9c229
Kozák Csaba <wondercsabo@VirtualBox.(none)>
2013-09-26 17:27:56 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudentAdmin::Application.config.secret_key_base = 'ce6651511a25319de249e5a9a947743da0726407d0ce251be5d198ec9cbe25ffc95a71eae9a48e20b7467a7398f503b47962da4af0831e15c483725ea4d3f9d1'
---
+++ spec/models/student_spec.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  nickname   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+require 'spec_helper'
+
+describe Student do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
