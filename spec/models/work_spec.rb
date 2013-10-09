c263b348d348936a662837ac1f3e95038a548126
WonderCsabo <kozakcsabi@gmail.com>
2013-10-09 11:09:34 +0200

    Added tests for Work and Student

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -3,6 +3,8 @@
 # Table name: works
 #
 #  id         :integer          not null, primary key
+#  task_id    :integer
+#  student_id :integer
 #  created_at :datetime
 #  updated_at :datetime
 #
@@ -10,5 +12,39 @@
 require 'spec_helper'
 
 describe Work do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @work = Work.new
+  end
+
+  subject { @work }
+
+  it { should respond_to(:task_id) }
+  it { should respond_to(:student_id) }
+  it { should respond_to(:created_at) }
+  it { should respond_to(:updated_at) }
+  it { should respond_to(:task) }
+  it { should respond_to(:student) }
+
+  it { should belong_to(:task) }
+  it { should belong_to(:student) }
+
+  it "should created when a task added to a student" do
+    student = Student.new
+    task = Task.new
+    student.tasks << task
+    student.save
+
+    work = Work.where(task_id: task.id, student_id: student.id)
+    expect(work).to be_true # not nil
+  end
+
+  it "should created when a student added to a task" do
+    student = Student.new
+    task = Task.new
+    task.students << student
+    task.save
+
+    work = Work.where(task_id: task.id, student_id: student.id)
+    expect(work).to be_true # not nil
+  end
 end
