9c370f556fbd5ac182966688c209fc265e4ca985
zerga1 <zerzagabor@gmail.com>
2013-10-24 16:52:59 +0200

    default business value again again

--- db/migrate/20131024140331_change_default_false_to_business.rb
+++ db/migrate/20131024140331_change_default_false_to_business.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultFalseToBusiness < ActiveRecord::Migration
   def change
-  	change_column :tasks, :business, :boolean, :null => false, :default => false
+  	change_column :tasks, :business, :boolean, :null => false, :default => false 
   end
 end
